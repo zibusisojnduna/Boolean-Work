5bcc15d07552bba9a1b68fa83013298e5ab7227b
User <User@WRSDLD309-040aa.dut.ac.za>
2024-08-21 09:54:34 +0200

    feat:edits

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Exercises() {
   return (
     <div>
-      <h1>This is the exercises page.</h1>
+      
     </div>
+    
   )
 }
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function NoPage() {
   return (
-    <div style={{backgroundImage:`url(${require("../assets/shutterstock_479042983.jpg")})`, height:"20", width:"20"}}></div>
+    <div style={{backgroundImage:`url(${require("../assets/shutterstock_479042983.jpg")})`, height:"40", width:"30", display:"flex", justifyContent:"center"}}></div>
   )
 }
