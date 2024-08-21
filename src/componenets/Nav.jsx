b5561c6d9114914be46f12f3691d6dcaced7de2a
User <User@WRSDLD309-040aa.dut.ac.za>
2024-08-21 15:33:34 +0200

    feat:landing added

--- src/componenets/Nav.jsx
+++ src/componenets/Nav.jsx
@@ -8,7 +8,8 @@ export default function Nav() {
   <Link to={"/TrafficLight"} className="w3-bar-item w3-button">Traffic Light</Link>
   <Link to={"/Exercises"} className="w3-bar-item w3-button">Exercises</Link>
   <Link to={"/NoPage"} className="w3-bar-item w3-button">Settings</Link>
-  <button sty>Log In</button>
+  <button style={{backgroundColor:"transparent", color:"white", borderRadius:"10%", border:"none",marginLeft:"23cm", cursor:"pointer"}}>Log In</button>
+  <button style={{backgroundColor:"white", borderRadius:"7%", cursor:"pointer", border:"none" }}>Sign Up</button>
 </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      <h1>This is the Home Page.</h1>
+  <section style={{position:"relative", backgroundSize:"cover"}}>
+    <img></img>
+    <div style={{position:"absolute"}}>
+      <h1>Want to maximise the most out of your meeting?<br></br>Find out how today</h1>
     </div>
+  </section>
   )
 }
